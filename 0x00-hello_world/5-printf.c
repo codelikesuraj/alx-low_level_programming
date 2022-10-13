42148a4e00c2942aebb1c0274ac8f4bf0bff597c
Abdulbaki Suraj <codelikesuraj@gmail.com>
2022-10-13 12:01:42 -0700

    added 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints a phrase
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
