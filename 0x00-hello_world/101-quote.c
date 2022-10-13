c28a8257a8c213035eba27bbc4d22ba639989a9c
Abdulbaki Suraj <codelikesuraj@gmail.com>
2022-10-13 12:22:51 -0700

    updated 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,9 +3,7 @@
 
 /**
  *
- * main - prints exactly "and that piece of art is useful"
- * - Dora Korpar, 2015-10-19",
- * followed by a new line, to the standard error.
+ * main - prints some strings
  * Return: Always 0 (Success)
  */
 
