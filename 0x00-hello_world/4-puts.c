3abfd0c7fe1cdb9c4f52f57a43e2e45253af944b
Abdulbaki Adabara Suraj <codelikesuraj@gmail.com>
2022-10-13 19:56:25 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
-#include <string.h>
 
 /**
- *
- * main - Entry point
- *
- * Description: prints "\"Programming is like building a multilingual puzzle"
- *
- * Return: 0
- *
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
