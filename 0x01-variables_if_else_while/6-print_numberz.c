1ec5386113852dbd54c6d7d54d50f168c20c31d1
Abdulbaki Suraj <codelikesuraj@gmail.com>
2022-10-14 14:28:09 -0700

    updated 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - no description (testing this)
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	int numbers = 48;
+
+	while (numbers < 58)
+	{
+		putchar(numbers);
+		numbers++;
+	}
+	putchar('\n');
+
+	return (0);
+}
