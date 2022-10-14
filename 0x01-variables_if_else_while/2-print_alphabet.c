0a94c8c9ec8ce9f6767f47b467dcd7cdd43af5a5
Abdulbaki Suraj <codelikesuraj@gmail.com>
2022-10-14 13:58:01 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - no description (testing this)
+ * Return - Always (0) Success
+ */
+int main(void)
+{
+	for (char alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		putchar(alphabet);
+	putchar('\n');
+
+	return (0);
+}
