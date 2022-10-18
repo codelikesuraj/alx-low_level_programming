ca85c9c702e7778ddf037abfb16df240bc523d1e
Abdulbaki Suraj <codelikesuraj@gmail.com>
2022-10-18 10:55:51 -0700

    added 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabets
+ * in lower case
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char alphabets = [];
+	
+	for (int i = a; a <= z; a++)
+		_putchar(i);
+	_putchar('\n');
+}
