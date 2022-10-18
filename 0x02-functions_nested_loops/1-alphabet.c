77ad2fcb22f7b044c7138e5112fa4497fbf15826
Abdulbaki Suraj <codelikesuraj@gmail.com>
2022-10-18 11:05:00 -0700

    updated 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,8 +9,8 @@
 void print_alphabet(void)
 {
 	char alphabet = 'a';
-	
-	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+
+	for (alphabet; alphabet <= 'z'; alphabet++)
 		_putchar(alphabet);
 	_putchar('\n');
 }
