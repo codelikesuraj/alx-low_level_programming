7f58241a6a84f74899d2033a5ffedc0b961d955b
Abdulbaki Adabara Suraj <codelikesuraj@gmail.com>
2023-07-13 09:26:37 -0700

    Updated 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Prints a sentence
+ *
+ * Description: Prints a sentence followed by a new line.
+ * Return: 0 on success
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
