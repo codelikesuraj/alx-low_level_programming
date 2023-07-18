af2e5310e717ac3361cb78566a26111913d6e48a
Abdulbaki Adabara Suraj <codelikesuraj@gmail.com>
2023-07-18 10:06:59 -0700

    Updated 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,6 @@
 #include "main.h"
 
 /**
- * main - Print alphabet in lowercase
- *
- * Description: Prints the alphabets in lowercase
- * Return: Always 0.
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-/**
  * print_alphabet - Print alphabet in lowercase
  */
 void print_alphabet(void)
