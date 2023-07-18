283f1ee549abb6212b82587126eb88eb4f24b86d
Abdulbaki Adabara Suraj <codelikesuraj@gmail.com>
2023-07-18 13:52:05 -0700

    Updated 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,10 +9,10 @@ void jack_bauer(void)
 
 	for (a = '0'; a < '3'; a++)
 	{
-		if (a >= '2' && b > '3')
-			break;
 		for (b = '0'; b <= '9'; b++)
 		{
+			if (a >= '2' && b >= '4')
+				break;
 			for (c = '0'; c < '6'; c++)
 			{
 				for (d = '0'; d <= '9'; d++)
