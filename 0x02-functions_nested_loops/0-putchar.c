613d443c7d40e8acd35df4a046898e9a608da1c7
Abdulbaki Adabara Suraj <codelikesuraj@gmail.com>
2023-07-18 04:37:09 -0700

    Added 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <main.h>
+
+/**
+ * main - Prints '_putchar'
+ *
+ * Description: Prints '_putchar'
+ * Return: 0 on success
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('P');
+	_putchar('U');
+	_putchar('T');
+	_putchar('C');
+	_putchar('H');
+	_putchar('A');
+	_putchar('R');
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+|File|Description|
+|-|-|
+|[0-putchar](0-putchar)|Write a program that prints '\_putchar' followed by a newline.|
