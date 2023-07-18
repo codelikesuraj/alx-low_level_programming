74310913bd673cd5b2fc6eb3d2b9a371efafc77b
Abdulbaki Adabara Suraj <codelikesuraj@gmail.com>
2023-07-18 12:53:35 -0700

    updated 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,15 +8,9 @@
  */
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122)
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
 		return (1);
 	}
-
-	if (c >= 65 && c >= 90)
-	{
-		return (1);
-	}
-
 	return (0);
 }
