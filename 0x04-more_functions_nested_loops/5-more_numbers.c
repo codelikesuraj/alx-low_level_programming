5e41296c6c00db56ef5cf042cfd102da3ab6c3f0
Abdulbaki Adabara Suraj <codelikesuraj@gmail.com>
2023-07-21 11:53:49 -0700

    Updated 5-more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 0-14 10 times each on a separate line
+ */
+void more_numbers(void)
+{
+	int i, n;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			if (i > 10)
+				_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
