11474a4b91bf6e8fd910e25b9dbff02e19a0a34a
Abdulbaki Suraj <codelikesuraj@gmail.com>
2023-07-25 20:07:11 +0100

    Updated 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,25 +6,15 @@
  */
 void rev_string(char *s)
 {
-	char *temp;
+	char temp;
 	int i, n = 0;
 
-	*(temp + 1) = 'h';
-	*(temp + 2) = 'i';
 	while (s[n] != '\0')
-	{
 		n++;
+	for (i = 0; i < --n; i++)
+	{
+		temp = s[n];
+		s[n] = s[i];
+		s[i] = temp;
 	}
-
-	for (i = 0; i < n; i++)
-		s[n] = temp[n - i];
-
-	for (i = 0; i <= n; i++)
-		_putchar(s[n]);
-	_putchar('\n');
-	for (i = 0; i <= n; i++)
-		_putchar(temp[n]);
-	_putchar('\n');
-	_putchar('\n');
-
 }
