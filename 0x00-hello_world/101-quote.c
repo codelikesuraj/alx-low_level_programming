dcfb1d4f54ff9a452c752d1012635e70696b09a2
Abdulbaki Adabara Suraj <codelikesuraj@gmail.com>
2023-07-30 05:35:47 -0700

    Updated 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  * main - print to stderr
  * Return: always 0
  */
-int main (void)
+int main(void)
 {
 	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (0);
