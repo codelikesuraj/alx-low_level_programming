753ae3b1f50037fbbccc0905346a2921a95e4eb8
Abdulbaki Suraj <codelikesuraj@gmail.com>
2023-08-01 15:55:01 +0100

    Added directory 0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+char *cap_string(char *str);
+char *leet(char *str);
+char *rot13(char *str);
+char *string_toupper(char *str);
+int _putchar(char c);
+int _strcmp(char *s1, char *s2);
+void print_number(int n);
+void reverse_array(int *a, int n);
