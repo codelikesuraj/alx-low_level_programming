0d810616260e1121b433dfa9db0b96f951563b26
Abdulbaki Suraj <codelikesuraj@gmail.com>
2023-08-03 18:00:38 +0100

    Added 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,3 +5,4 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 int _putchar(char c);
 unsigned int _strspn(char *s, char *accept);
+void print_chessboard(char (*a)[8]);
