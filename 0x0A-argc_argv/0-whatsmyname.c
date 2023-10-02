516e86852aa176bcbac490fc7d6792ce7378bab0
Abdulbaki Suraj <codelikesuraj@gmail.com>
2023-10-02 16:56:26 +0100

    Updated 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,10 +3,14 @@
 
 /**
  * main - prints the name of the file
+ * @argc: number of arguments
+ * @argv: array of arguments
  * Return: Always '0'.
  */
 int main(int argc, char *argv[])
 {
+	(void)argc;
 	_puts(argv[0]);
+	_putchar('\n');
 	return (0);
 }
--- 0x0A-argc_argv/_puts.c
+++ 0x0A-argc_argv/_puts.c
@@ -11,5 +11,4 @@ void _puts(char *str)
 	{
 		_putchar(*str);
 	}
-	_putchar('\n');
 }
