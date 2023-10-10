54cc6d2f30f0dea114ab2a778482e440ed2276c0
Abdulbaki Suraj <codelikesuraj@gmail.com>
2023-10-10 21:56:21 +0100

    Added dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+|File|Description|
+|-|-|
+|[dog-h](dog.h)|A new type 'struct dog' with the following elements: 'name' of type 'char \*', 'age' of type 'float' & 'owner' of type 'char \*'.|
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - A dog struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of the owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
