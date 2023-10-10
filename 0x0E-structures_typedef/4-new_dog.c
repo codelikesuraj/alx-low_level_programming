4a7aee4563844be190c3691b5f09f84756ce1abd
Abdulbaki Suraj <codelikesuraj@gmail.com>
2023-10-10 23:38:02 +0100

    Added 4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - initializes a new instance
+ * of type dog_t
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
+	return (dog);
+}
