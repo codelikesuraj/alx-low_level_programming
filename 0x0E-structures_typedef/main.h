061490c35ad44578568b0ff52beaeed0f54bb1b1
Abdulbaki Suraj <codelikesuraj@gmail.com>
2023-10-10 22:40:34 +0100

    Updated dog.h

--- 0x0E-structures_typedef/main.h
+++
@@ -1,8 +0,0 @@
-#ifndef _DOG_H_
-#define _DOG_H_
-#endif /* _DOG_H_ */
-
-#ifndef _MAIN_H_
-#define _MAIN_H_
-void init_dog(struct dog *d, char *name, float age, char *owner);
-#endif /* _MAIN_H_ */
