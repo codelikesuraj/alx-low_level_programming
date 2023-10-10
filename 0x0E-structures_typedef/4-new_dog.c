45fed093f8e093c5bf8e383e986a874422eccb34
Abdulbaki Suraj <codelikesuraj@gmail.com>
2023-10-10 23:41:00 +0100

    Updated 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -7,6 +7,8 @@
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
+ * Return: pointer to the new dog_t or
+ * NULL on errro;
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
