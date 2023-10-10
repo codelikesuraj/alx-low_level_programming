d1451b82ab49974ccb73ba4de85f889107cb3c3b
Abdulbaki Suraj <codelikesuraj@gmail.com>
2023-10-10 20:39:18 +0100

    Updated 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
+#ifndef 3_FUNCTION_LIKE_MACRO_H
+#define 3_FUNCTION_LIKE_MACRO_H
 #define ABS(x) (((x) < 0) ? (-x) : (x))
-#endif /* FUNCTION_LIKE_MACRO_H */
+#endif /* 3_FUNCTION_LIKE_MACRO_H */
