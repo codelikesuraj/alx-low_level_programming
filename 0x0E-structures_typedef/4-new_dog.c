49216df6d740c5a882f363713a1ad8be3adbff3d
Abdulbaki Suraj <codelikesuraj@gmail.com>
2023-10-11 00:58:58 +0100

    updated 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -34,7 +34,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 
 	dog->owner = malloc((len_owner + 1) * sizeof(char));
-	if (dog->name == NULL && dog->owner == NULL)
+	if (dog->owner == NULL)
 	{
 		free(dog->name);
 		free(dog);
