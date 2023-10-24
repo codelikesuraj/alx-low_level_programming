5d1fc6669d29f3ddb200d598b5bdbb08e37c6fd9
Abdulbaki Suraj <codelikesuraj@gmail.com>
2023-10-24 21:52:13 +0100

    Updated 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,14 +6,13 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t **temp;
+	listint_t *next;
 
-	temp = head;
 	if (*head)
 	{
-		free_listint2(&(*head)->next);
+		next = (*head)->next;
+		free_listint2(&next);
 		free(*head);
-	}
-
-	*temp = NULL;
+	}	
+	*head = NULL;
 }
